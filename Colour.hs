ae5a52f4449be28bd13b1dfbfa9e1b0a604647f3
vi <noreply@example.org>
2014-01-24 19:55:37 +0800

    Context (working space, reference white) passable to conversion functions.

--- Colour.hs
+++ Colour.hs
@@ -12,9 +12,9 @@ data XYZ    = XYZ    (Matrix ℝ) deriving (Show)
 data RGB    = RGB    (Matrix ℝ) deriving (Show)
 data CIELAB = CIELAB (Matrix ℝ) deriving (Show)
 
-data WS = WS
+data Workspace = Workspace
   { gamma :: ℝ
-  , white :: XYZ -- reference white
+  , white :: White -- reference white
   , red   :: Primary
   , green :: Primary
   , blue  :: Primary
@@ -26,15 +26,17 @@ data Primary = Primary
   , z :: ℝ 
   }
 
-d65 :: XYZ
+type White = XYZ
+
+d65 :: White
 d65 = XYZ . M.fromList . map pure $
   [ 0.95047
   , 1
   , 1.08883
   ]
 
-adobeRGB1998 :: WS
-adobeRGB1998 = WS
+adobeRGB1998 :: Workspace
+adobeRGB1998 = Workspace
   { gamma = 2.2
   , white = d65
   , red   = Primary 0.64 0.33 0.297361
@@ -42,7 +44,7 @@ adobeRGB1998 = WS
   , blue  = Primary 0.15 0.06 0.075285
   }
 
-rgbM :: WS -> Matrix ℝ
+rgbM :: Workspace -> Matrix ℝ
 rgbM ws = M.fromList $
   [ [sr*xr, sg*xg, sb*xb]
   , [sr*yr, sg*yg, sb*yb]
@@ -74,7 +76,7 @@ rgbM ws = M.fromList $
                        . white 
                        $ ws
 
-rgbM' :: WS -> Matrix ℝ
+rgbM' :: Workspace -> Matrix ℝ
 rgbM' = fromJust
       . inv 
       . rgbM
@@ -84,49 +86,50 @@ fs <***> m = fs
          <*> pure m
 
 -- Maximum CIE stimulus value for the space in some given dimension.
-triMax :: WS -> (WS -> Primary) -> (ℝ,ℝ,ℝ)
+triMax :: Workspace -> (Workspace -> Primary) -> (ℝ,ℝ,ℝ)
 triMax ws g = ( x0/y0
               , 1
               , (1-x0-y0)/y0
               )
   where [x0, y0] = [x, y] <***> g ws
 
+data Context = Reference White 
+             | Space Workspace
+
 class Colour a where
-  toXYZ :: a -> XYZ
-  toRGB :: a -> RGB
-  toRGB = toRGB . toXYZ
-  toCIELAB :: a -> CIELAB
-  toCIELAB = toCIELAB . toXYZ
+  toXYZ :: Context -> a -> XYZ
+  toRGB :: Context -> a -> RGB
+  toRGB c = toRGB c . toXYZ c
+  toCIELAB :: Context -> a -> CIELAB
+  toCIELAB c = toCIELAB c . toXYZ c
 
 instance Colour XYZ where
-  toXYZ = id
-  toRGB (XYZ xyz) = RGB 
-                  $ rgbM' adobeRGB1998
+  toXYZ _ xyz = xyz
+  toRGB (Space ws) (XYZ xyz) = RGB 
+                  $ rgbM' ws
                   * xyz
-  toCIELAB (XYZ xyz) = CIELAB . M.fromList . pure $
+  toCIELAB (Reference wt) (XYZ xyz) = CIELAB . M.fromList . pure $
     [ 116 * f(y/y') - 16
     , 500 * (f(x/x') - f(y/y'))
     , 200 * (f(y/y') - f(z/z'))
     ]
     where [x,y,z]    = col 1 xyz 
-          [x',y',z'] = col 1 $ (\(XYZ x)-> x) d65
+          [x',y',z'] = col 1 $ (\(XYZ x)-> x) wt
           f t | t > (6/29)**3 = t**(1/3)
               | otherwise     = (t/3)*((29/6)**2) + 4/29
 
 instance Colour RGB where
-  toRGB = id
-  toXYZ (RGB rgb) = XYZ 
-                  $ rgbM adobeRGB1998
-                  * rgb
+  toXYZ (Space ws) (RGB rgb) = XYZ 
+                             $ rgbM ws
+                             * rgb
 
 instance Colour CIELAB where
-  toCIELAB = id
-  toXYZ (CIELAB lab) = XYZ . M.fromList . pure $
+  toXYZ (Reference wt) (CIELAB lab) = XYZ . M.fromList . pure $
     [ x' * f'(1/116 * (l+16) + a/500)
     , y' * f'(1/116 * (l+16))
     , z' * f'(1/116 * (l+16) - b/200)
     ]
     where [l,a,b] = col 1 lab
-          [x',y',z'] = col 1 $ (\(XYZ x)-> x) d65
+          [x',y',z'] = col 1 $ (\(XYZ x)-> x) wt
           f' t | t > 6/29  = t**3
                | otherwise = 3*(6/29)**2 * (t-4/29)
