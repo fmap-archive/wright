2ec068e41fe73f78962bceb007a80dfa4560bb1a
vi <noreply@example.org>
2014-01-24 19:32:11 +0800

    XYZ <-> CIEL*a*b*

--- Colour.hs
+++ Colour.hs
@@ -8,8 +8,9 @@ import Data.Maybe (fromJust)
 type ℝ = Double
 
 -- Each three-row vectors:
-data XYZ = XYZ (Matrix ℝ) deriving (Show)
-data RGB = RGB (Matrix ℝ) deriving (Show)
+data XYZ    = XYZ    (Matrix ℝ) deriving (Show)
+data RGB    = RGB    (Matrix ℝ) deriving (Show)
+data CIELAB = CIELAB (Matrix ℝ) deriving (Show)
 
 data WS = WS
   { gamma :: ℝ
@@ -94,15 +95,38 @@ class Colour a where
   toXYZ :: a -> XYZ
   toRGB :: a -> RGB
   toRGB = toRGB . toXYZ
+  toCIELAB :: a -> CIELAB
+  toCIELAB = toCIELAB . toXYZ
 
 instance Colour XYZ where
   toXYZ = id
   toRGB (XYZ xyz) = RGB 
                   $ rgbM' adobeRGB1998
                   * xyz
+  toCIELAB (XYZ xyz) = CIELAB . M.fromList . pure $
+    [ 116 * f(y/y') - 16
+    , 500 * (f(x/x') - f(y/y'))
+    , 200 * (f(y/y') - f(z/z'))
+    ]
+    where [x,y,z]    = col 1 xyz 
+          [x',y',z'] = col 1 $ (\(XYZ x)-> x) d65
+          f t | t > (6/29)**3 = t**(1/3)
+              | otherwise     = (t/3)*((29/6)**2) + 4/29
 
 instance Colour RGB where
   toRGB = id
   toXYZ (RGB rgb) = XYZ 
                   $ rgbM adobeRGB1998
                   * rgb
+
+instance Colour CIELAB where
+  toCIELAB = id
+  toXYZ (CIELAB lab) = XYZ . M.fromList . pure $
+    [ x' * f'(1/116 * (l+16) + a/500)
+    , y' * f'(1/116 * (l+16))
+    , z' * f'(1/116 * (l+16) - b/200)
+    ]
+    where [l,a,b] = col 1 lab
+          [x',y',z'] = col 1 $ (\(XYZ x)-> x) d65
+          f' t | t > 6/29  = t**3
+               | otherwise = 3*(6/29)**2 * (t-4/29)
