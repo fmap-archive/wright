5ec74545ca08929ee9b9655809dd40336e52d6ca
vi <noreply@example.org>
2014-01-27 18:23:02 +0800

    Factored RGB M code

--- Data/Colour/RGB/Matrix.hs
+++ Data/Colour/RGB/Matrix.hs
@@ -1,6 +1,6 @@
-module Data.Colour.RGB.Matrix where
+module Data.Colour.RGB.Matrix (m, m') where
 
-import Numeric.Matrix (inv, Matrix(..), col)
+import Numeric.Matrix (inv, Matrix(..), col, transpose)
 import qualified Numeric.Matrix as M (fromList, toList)
 import Data.Maybe (fromJust)
 import Control.Applicative ((<$>), pure)
@@ -12,41 +12,36 @@ m ws = M.fromList $
   [ [sr*xr, sg*xg, sb*xb]
   , [sr*yr, sg*yg, sb*yb]
   , [sr*zr, sg*zg, sb*zb]
-  ] where [(xr, yr, zr),
-           (xg, yg, zg),
-           (xb, yb, zb)] = triMax ws 
-                       <$> [ red
-                           , green
-                           , blue
-                           ] 
-          [sr, sg, sb] = col 1 
-                       $ ( fromJust
-                         . inv 
-                         $ M.fromList 
-                            [ [xr, xg, xb]
-                            , [yr, yg, yb]
-                            , [zr, zg, zb]
-                            ]
-                         )
-                       * ( M.fromList 
-                         . map pure 
-                         $ [ xw
-                           , yw
-                           , zw
-                           ]
-                         )
-          [xw, yw, zw] = (\(XYZ m)-> concat . M.toList $ m)
-                       . white 
-                       $ ws
-
-
--- Maximum CIE stimulus value for the space in some given dimension.
-triMax :: Workspace -> (Workspace -> Primary) -> (ℝ,ℝ,ℝ)
-triMax ws g = ( x0/y0
-              , 1
-              , (1-x0-y0)/y0
-              )
-  where [x0, y0] = [x, y] <***> g ws
+  ] where [sr, sg, sb] = sPoint ws
+          [[xr, yr, zr],
+           [xg, yg, zg],
+           [xb, yb, zb]] = chromCoords ws
 
 m' :: Workspace -> Matrix ℝ
 m' = fromJust . inv . m
+
+sPoint :: Workspace -> [ℝ]
+sPoint ws = col 1 
+          $ ( fromJust
+            . inv
+            . transpose
+            . M.fromList
+            . chromCoords
+            $ ws
+            )
+          * whitePoint ws
+
+chromCoords :: Workspace -> [[ℝ]]
+chromCoords ws = triMax ws 
+             <$> [red, green, blue]
+
+whitePoint :: Workspace -> Matrix ℝ
+whitePoint = M.fromList . map pure . xyzToList . white
+
+xyzToList :: XYZ -> [ℝ]
+xyzToList (XYZ xyz) = concat . M.toList $ xyz
+
+-- Maximum CIE stimulus value for the space in some given dimension.
+triMax :: Workspace -> (Workspace -> Primary) -> [ℝ]
+triMax ws g = [x0/y0 , 1 , (1-x0-y0)/y0]
+  where [x0, y0] = [x, y] <***> g ws
