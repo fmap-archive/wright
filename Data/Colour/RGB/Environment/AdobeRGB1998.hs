7855dee7c66db8f05386391d481575fb9a79a6e9
vi <noreply@example.org>
2014-01-29 20:42:38 +0800

    Rename RGB.WorkingSpace RGB.Environment, repair the use of the illuminant in AdobeRGB1998.

--- Data/Colour/RGB/Environment/AdobeRGB1998.hs
+++ Data/Colour/RGB/Environment/AdobeRGB1998.hs
@@ -1,12 +1,11 @@
-module Data.Colour.RGB.WorkingSpace.AdobeRGB1998 where 
+module Data.Colour.RGB.Environment.AdobeRGB1998 (adobeRGB1998) where 
 
 import Data.Colour.Types (Environment(..), Primary(..))
 import Data.Colour.CIE.Illuminant (d65)
 
 adobeRGB1998 :: Environment
-adobeRGB1998 = Environment
+adobeRGB1998 = d65
   { gamma = 2.2
-  , white = d65
   , red   = Primary 0.64 0.33 0.297361
   , green = Primary 0.21 0.71 0.627355
   , blue  = Primary 0.15 0.06 0.075285
