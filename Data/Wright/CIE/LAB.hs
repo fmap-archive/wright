cf060aad13cae7ff4d5d8aad423bfec0989f6fa4
vi <noreply@example.org>
2014-02-06 11:47:39 +0800

    Working LAB --> XYZ

--- Data/Wright/CIE/LAB.hs
+++ Data/Wright/CIE/LAB.hs
@@ -3,19 +3,19 @@ module Data.Wright.CIE.LAB where
 import Data.Wright
 import Data.Wright.Types
 import Data.Wright.CIE.XYZ
-import Numeric.Matrix(col)
+import Numeric.Matrix (col)
 import qualified Numeric.Matrix as M (fromList)
-import Control.Applicative (pure)
 
 instance Colour CIELAB where
-  toXYZ (Model _ wt _ _ _) (CIELAB lab) = XYZ . M.fromList . map pure $
-    [ x' * f'(1/116 * (l+16) + a/500)
-    , y' * f'(1/116 * (l+16))
-    , z' * f'(1/116 * (l+16) - b/200)
-    ]
-    where [l,a,b] = col 1 lab
-          [x',y',z'] = col 1 . acc $ wt
-          f' t | t > 6/29  = t**3
-               | otherwise = 3*(6/29)**2 * (t-4/29)
   acc (CIELAB lab) = lab
   cure = CIELAB
+  toXYZ (Model _ wt _ _ _) (CIELAB lab) = XYZ . M.fromList . map return $
+    [ xw * if fx**3>k then fx**3 else (116*fx-16)/j
+    , yw * if l > 8 then ((l+16)/116)**3 else l/j
+    , zw * if fz**4 > k then fz**3 else (116*fz-16)/j
+    ] where [l, a, b] = col 1 lab
+            [xw,yw,zw] = col 1 . acc $ wt
+            fy = (l+16)/116
+            fx = 2e-3 * a + fy
+            fz = fy - 5e-3 * b
+            (k, j) = (216/24389, 24389/27)
