e243c117e200b51cef58c6599b0233a3840581fe
vi <noreply@example.org>
2014-02-07 20:58:03 +0800

    Export all RGB working spaces through RGB.Model

--- Data/Wright.hs
+++ Data/Wright.hs
@@ -24,29 +24,13 @@ module Data.Wright
 , module Data.Wright.CIE.Illuminant.F7
 , module Data.Wright.CIE.Illuminant.F8
 , module Data.Wright.CIE.Illuminant.F9
-, module Data.Wright.RGB.Model.AdobeRGB1998
-, module Data.Wright.RGB.Model.AppleRGB
-, module Data.Wright.RGB.Model.BestRGB
-, module Data.Wright.RGB.Model.BetaRGB
-, module Data.Wright.RGB.Model.BruceRGB
-, module Data.Wright.RGB.Model.CIERGB
-, module Data.Wright.RGB.Model.ColorMatchRGB
-, module Data.Wright.RGB.Model.DonRGB4
-, module Data.Wright.RGB.Model.ECIRGBv2
-, module Data.Wright.RGB.Model.EktaSpacePS5
-, module Data.Wright.RGB.Model.LabGamut
-, module Data.Wright.RGB.Model.NTSCRGB
-, module Data.Wright.RGB.Model.PALSECAMRGB
-, module Data.Wright.RGB.Model.ProPhotoRGB
-, module Data.Wright.RGB.Model.SMPTECRGB
-, module Data.Wright.RGB.Model.SRGB
-, module Data.Wright.RGB.Model.WideGamutRGB
 , module Data.Wright.CIE.DeltaE
 ) where
 
 import  Data.Wright.Colour
 import  Data.Wright.Types
 import  Data.Wright.RGB
+import  Data.Wright.RGB.Model
 import  Data.Wright.CIE.XYZ
 import  Data.Wright.CIE.LAB
 import  Data.Wright.CIE.Illuminant.A
@@ -69,21 +53,4 @@ import  Data.Wright.CIE.Illuminant.F6
 import  Data.Wright.CIE.Illuminant.F7
 import  Data.Wright.CIE.Illuminant.F8
 import  Data.Wright.CIE.Illuminant.F9
-import  Data.Wright.RGB.Model.AdobeRGB1998
-import  Data.Wright.RGB.Model.AppleRGB
-import  Data.Wright.RGB.Model.BestRGB
-import  Data.Wright.RGB.Model.BetaRGB
-import  Data.Wright.RGB.Model.BruceRGB
-import  Data.Wright.RGB.Model.CIERGB
-import  Data.Wright.RGB.Model.ColorMatchRGB
-import  Data.Wright.RGB.Model.DonRGB4
-import  Data.Wright.RGB.Model.ECIRGBv2
-import  Data.Wright.RGB.Model.EktaSpacePS5
-import  Data.Wright.RGB.Model.LabGamut
-import  Data.Wright.RGB.Model.NTSCRGB
-import  Data.Wright.RGB.Model.PALSECAMRGB
-import  Data.Wright.RGB.Model.ProPhotoRGB
-import  Data.Wright.RGB.Model.SMPTECRGB
-import  Data.Wright.RGB.Model.SRGB
-import  Data.Wright.RGB.Model.WideGamutRGB
 import  Data.Wright.CIE.DeltaE
---
+++ Data/Wright/RGB/Model.hs
@@ -0,0 +1,37 @@
+module Data.Wright.RGB.Model
+( module Data.Wright.RGB.Model.AdobeRGB1998
+, module Data.Wright.RGB.Model.AppleRGB
+, module Data.Wright.RGB.Model.BestRGB
+, module Data.Wright.RGB.Model.BetaRGB
+, module Data.Wright.RGB.Model.BruceRGB
+, module Data.Wright.RGB.Model.CIERGB
+, module Data.Wright.RGB.Model.ColorMatchRGB
+, module Data.Wright.RGB.Model.DonRGB4
+, module Data.Wright.RGB.Model.ECIRGBv2
+, module Data.Wright.RGB.Model.EktaSpacePS5
+, module Data.Wright.RGB.Model.LabGamut
+, module Data.Wright.RGB.Model.NTSCRGB
+, module Data.Wright.RGB.Model.PALSECAMRGB
+, module Data.Wright.RGB.Model.ProPhotoRGB
+, module Data.Wright.RGB.Model.SMPTECRGB
+, module Data.Wright.RGB.Model.SRGB
+, module Data.Wright.RGB.Model.WideGamutRGB
+) where
+
+import  Data.Wright.RGB.Model.AdobeRGB1998
+import  Data.Wright.RGB.Model.AppleRGB
+import  Data.Wright.RGB.Model.BestRGB
+import  Data.Wright.RGB.Model.BetaRGB
+import  Data.Wright.RGB.Model.BruceRGB
+import  Data.Wright.RGB.Model.CIERGB
+import  Data.Wright.RGB.Model.ColorMatchRGB
+import  Data.Wright.RGB.Model.DonRGB4
+import  Data.Wright.RGB.Model.ECIRGBv2
+import  Data.Wright.RGB.Model.EktaSpacePS5
+import  Data.Wright.RGB.Model.LabGamut
+import  Data.Wright.RGB.Model.NTSCRGB
+import  Data.Wright.RGB.Model.PALSECAMRGB
+import  Data.Wright.RGB.Model.ProPhotoRGB
+import  Data.Wright.RGB.Model.SMPTECRGB
+import  Data.Wright.RGB.Model.SRGB
+import  Data.Wright.RGB.Model.WideGamutRGB
